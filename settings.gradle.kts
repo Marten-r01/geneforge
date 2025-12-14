3adb79902b188c568f8ba4ed9fae2acded1d5651
Раиса Чухнова <Raya.chuhnova@yandex.ru>
2025-12-14 15:32:04 +0300

    Configure root project and include services

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
-
+rootProject.name = "gene-forge"
+include(
+    "services:api-gateway",
+    "services:config-service",
+    "services:service-discovery",
+    "services:user-service",
+    "services:genetic-service",
+    "services:model-service"
+)
